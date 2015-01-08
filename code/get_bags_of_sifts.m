e7c7581b46d636efcb94e4affbcda4bf4b8e14ae
Trung Tran <tqt5nt@virginia.edu>
2015-01-07 23:02:24 -0500

    Clean up boiler-plate comments

--- code/get_bags_of_sifts.m
+++ code/get_bags_of_sifts.m
@@ -1,28 +1,7 @@
-% Starter code prepared by James Hays for CS 143, Brown University
-
-%This feature representation is described in the handout, lecture
-%materials, and Szeliski chapter 14.
 
 function image_feats = get_bags_of_sifts(image_paths)
 % image_paths is an N x 1 cell array of strings where each string is an
 % image path on the file system.
-
-% This function assumes that 'vocab.mat' exists and contains an N x 128
-% matrix 'vocab' where each row is a kmeans centroid or visual word. This
-% matrix is saved to disk rather than passed in a parameter to avoid
-% recomputing the vocabulary every time at significant expense.
-
-% image_feats is an N x d matrix, where d is the dimensionality of the
-% feature representation. In this case, d will equal the number of clusters
-% or equivalently the number of entries in each image's histogram.
-
-% You will want to construct SIFT features here in the same way you
-% did in build_vocabulary.m (except for possibly changing the sampling
-% rate) and then assign each local feature to its nearest cluster center
-% and build a histogram indicating how many times each cluster was used.
-% Don't forget to normalize the histogram, or else a larger image with more
-% SIFT features will look very different from a smaller version of the same
-% image.
 
     % vocab = vocab_size x 128
     load('vocab.mat')
